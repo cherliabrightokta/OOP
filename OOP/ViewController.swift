5dd992cc57a24b859cf107cc3165c57a41825555
Cherlia Brightokta <cherlia0610@gmail.com>
2019-05-15 13:21:13 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Cherlia Brightokta on 15/05/19.
+//  Copyright © 2019 Cherlia Brightokta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
